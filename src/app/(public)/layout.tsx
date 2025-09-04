7e52f8fe61fee4443766899af9c27fc72ec22088
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-09-04 05:48:28 +0000

    Seems the header and footer is vanished

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,14 +1,10 @@
-import PublicFooter from "@/components/app/public-footer";
-import PublicHeader from "@/components/app/public-header";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-muted min-h-screen flex flex-col">
-        <PublicHeader />
         <main className="flex-1">
             {children}
         </main>
-        <PublicFooter />
     </div>
   );
 }
