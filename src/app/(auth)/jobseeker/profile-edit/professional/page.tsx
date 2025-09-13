dc831f8c2785cde884817b47db80648b5372534e
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-09-13 13:08:22 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/(auth)/jobseeker/profile-edit/professional/page.tsx
+++ src/app/(auth)/jobseeker/profile-edit/professional/page.tsx
@@ -1,4 +1,4 @@
-import { ProfileFormProfessional } from '@/components/app/candidate/profile-forms/professional';
+import { ProfileFormProfessional } from '@/components/app/jobseeker/profile-forms/professional';
 import { candidates } from '@/lib/data';
 
 export default function JobseekerProfileProfessionalPage() {
