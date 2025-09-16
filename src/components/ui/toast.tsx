2dd578bb07b08955545fc3942f73478d2b91f45a
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-09-16 10:28:47 +0000

    Change the color combination for the toast.

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -29,11 +30,11 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "bg-primary text-primary-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
         success:
-          "success group border-emerald-500 bg-emerald-500 text-white",
+          "success group border-transparent bg-accent text-accent-foreground",
       },
     },
     defaultVariants: {
@@ -66,7 +67,7 @@ const ToastAction = React.forwardRef<
     className={cn(
       "inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
       "group-[.destructive]:border-muted/40 group-[.destructive]:hover:border-destructive/30 group-[.destructive]:hover:bg-destructive group-[.destructive]:hover:text-destructive-foreground group-[.destructive]:focus:ring-destructive",
-      "group-[.success]:border-muted/40 group-[.success]:hover:border-emerald-500/30 group-[.success]:hover:bg-emerald-500 group-[.success]:hover:text-white group-[.success]:focus:ring-emerald-500",
+       "group-[.success]:border-muted/40 group-[.success]:hover:border-accent/30 group-[.success]:hover:bg-accent group-[.success]:hover:text-accent-foreground group-[.success]:focus:ring-accent",
       className
     )}
     {...props}
@@ -83,7 +84,7 @@ const ToastClose = React.forwardRef<
     className={cn(
       "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100",
       "group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
-       "group-[.success]:text-emerald-300 group-[.success]:hover:text-emerald-50 group-[.success]:focus:ring-emerald-400 group-[.success]:focus:ring-offset-emerald-600",
+       "group-[.success]:text-green-300 group-[.success]:hover:text-green-50 group-[.success]:focus:ring-green-400 group-[.success]:focus:ring-offset-green-600",
       className
     )}
     toast-close=""
