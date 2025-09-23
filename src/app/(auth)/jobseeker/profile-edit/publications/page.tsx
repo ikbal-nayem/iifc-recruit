a26b7862bc9808b630177208135483bd3258172f
ikbal-nayem <ikbalnayem000@gmail.com>
2025-09-23 21:20:00 +0600

    url change

--- src/app/(auth)/jobseeker/profile-edit/publications/page.tsx
+++ src/app/(auth)/jobseeker/profile-edit/publications/page.tsx
@@ -1,8 +1,5 @@
 import { ProfileFormPublications } from '@/components/app/jobseeker/profile-forms/publications';
-import { candidates } from '@/lib/data';
 
 export default function JobseekerProfilePublicationsPage() {
-  const candidate = candidates[0];
-
-  return <ProfileFormPublications candidate={candidate} />;
+	return <ProfileFormPublications />;
 }
--- src/services/api/jobseeker-profile.service.ts
+++ src/services/api/jobseeker-profile.service.ts
@@ -4,14 +4,17 @@ import { axiosIns } from '@/config/api.config';
 import { IApiResponse } from '@/interfaces/common.interface';
 import { Publication } from '@/lib/types';
 
-const createProfileCrud = <T extends { id?: string }>(entity: string) => ({
-	get: async (): Promise<IApiResponse<T[]>> => await axiosIns.get(`/jobseeker/profile/${entity}`),
+const createProfileCrud = <T>(entity: string) => ({
+	get: async (): Promise<IApiResponse<T[]>> => await axiosIns.get(`/jobseeker/${entity}/get`),
+	
 	add: async (payload: Omit<T, 'id'>): Promise<IApiResponse<T>> =>
-		await axiosIns.post(`/jobseeker/profile/${entity}`, payload),
+		await axiosIns.post(`/jobseeker/${entity}/create`, payload),
+	
 	update: async (payload: T): Promise<IApiResponse<T>> =>
-		await axiosIns.put(`/jobseeker/profile/${entity}/${payload.id}`, payload),
+		await axiosIns.put(`/jobseeker/${entity}/update`, payload),
+
 	delete: async (id: string): Promise<IApiResponse<void>> =>
-		await axiosIns.delete(`/jobseeker/profile/${entity}/${id}`),
+		await axiosIns.delete(`/jobseeker/${entity}/delete/${id}`),
 });
 
 export const JobseekerProfileService = {
