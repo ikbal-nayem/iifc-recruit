de27148602a28641c6ff51fdb5d3d3f3b8094bbc
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-09-24 04:34:35 +0000

    Type '"warning"' is not assignable to type '"link" | "default" | "danger

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,6 +12,8 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         danger:
           "bg-danger text-danger-foreground hover:bg-danger/90",
+        warning:
+          "bg-warning text-warning-foreground hover:bg-warning/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
