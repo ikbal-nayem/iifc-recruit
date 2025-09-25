ee3be40412ffe1d104355946ba899d9b5d0d2843
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-09-25 16:47:27 +0000

    nn

--- src/app/(auth)/jobseeker/profile-edit/awards/page.tsx
+++ src/app/(auth)/jobseeker/profile-edit/awards/page.tsx
@@ -1,8 +1,6 @@
 import { ProfileFormAwards } from '@/components/app/jobseeker/profile-forms/awards';
-import { candidates } from '@/lib/data';
 
 export default function JobseekerProfileAwardsPage() {
-  const candidate = candidates[0];
 
-  return <ProfileFormAwards candidate={candidate} />;
+  return <ProfileFormAwards  />;
 }
