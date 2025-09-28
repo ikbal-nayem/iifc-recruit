395ec1a7792ad313c7ca9b6086c8e63743e314d3
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-09-28 07:22:21 +0000

    There is a extra "X" in the modal. (_for element <X>_)

--- src/components/ui/file-previewer.tsx
+++ src/components/ui/file-previewer.tsx
@@ -131,9 +131,6 @@ export function FilePreviewer({ file, children, className }: FilePreviewerProps)
 							>
 								{isMaximized ? <Minimize className='h-4 w-4' /> : <Maximize className='h-4 w-4' />}
 							</Button>
-							<Button variant='ghost' size='icon' className='h-8 w-8' onClick={handleClose}>
-								<X className='h-4 w-4' />
-							</Button>
 						</div>
 					</DialogHeader>
 
