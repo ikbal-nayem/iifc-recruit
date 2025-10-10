bd5126c3e008d52b632653bc80111db6339ce3e1
ikbal-nayem <ikbalnayem000@gmail.com>
2025-10-10 10:02:09 +0600

    nn

--- src/components/ui/action-menu.tsx
+++ src/components/ui/action-menu.tsx
@@ -76,7 +76,6 @@ export function ActionMenu({ items, trigger, label }: ActionMenuProps) {
 			<DropdownMenuTrigger asChild>
 				{trigger || (
 					<Button variant='ghost' className='h-8 w-8 p-0'>
-						<span className='sr-only'>Open menu</span>
 						<MoreHorizontal className='h-4 w-4' />
 					</Button>
 				)}
