6fd0e69c0a4b9479438b612e8138c6ac6f3d5c0d
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-10-11 13:39:59 +0000

    The toast can not show full message description. The message is getting

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -19,7 +19,7 @@ export function Toaster() {
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="grid gap-1">
+            <div className="flex-1 break-words">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
