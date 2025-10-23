9287dc704fe04ae8d78fc78e54a2c42d3397c3c5
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-10-23 09:10:21 +0000

    Remove unnecessary files and folders

--- src/app/(auth)/admin/job-management/request/[id]/manage/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RequestManagementPage } from '@/components/app/admin/job-management/request-management-page';
-
-export default function ManageJobRequestPage({ params }: { params: { id: string } }) {
-	return <RequestManagementPage requestId={params.id} />;
-}
