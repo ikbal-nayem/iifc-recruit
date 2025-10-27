dda16ce52a6ff7679cb28626b45fd7e4164fc15c
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-10-27 16:14:54 +0000

    login url

--- src/services/api/auth.service.ts
+++ src/services/api/auth.service.ts
@@ -5,10 +5,10 @@ import { IApiResponse, IObject } from '@/interfaces/common.interface';
 
 export const AuthService = {
 	login: async (payload: { email: string; password: string }): Promise<IApiResponse<IAuthInfo>> =>
-		await axiosIns.post('/auth/user/login', payload),
+		await axiosIns.post('/auth/login', payload),
 
 	signup: async (payload: IObject): Promise<IApiResponse> =>
-		await axiosIns.post('/auth/user/signup', payload),
+		await axiosIns.post('/auth/signup', payload),
 	
 	getUserProfile: async (): Promise<IApiResponse<IUser>> =>
 		await axiosIns.get('/auth/user'),
