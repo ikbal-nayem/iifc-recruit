cb1e1664f82f834e4cddfc9d2d1b9b18524e7f4e
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-11-04 09:54:47 +0000

    Show realtime character count for textarea.

--- src/components/ui/form-textarea.tsx
+++ src/components/ui/form-textarea.tsx
@@ -3,10 +3,11 @@
 
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
 import * as React from 'react';
 import { Control, FieldPath, FieldValues } from 'react-hook-form';
 
-interface FormTextareaProps<TFieldValues extends FieldValues> {
+interface FormTextareaProps<TFieldValues extends FieldValues> extends React.ComponentProps<'textarea'> {
 	control: Control<TFieldValues | any>;
 	name: FieldPath<TFieldValues>;
 	label?: string;
@@ -22,21 +23,39 @@ export function FormTextarea<TFieldValues extends FieldValues>({
 	placeholder,
 	rows = 2,
 	required = false,
+	maxLength,
 	...props
 }: FormTextareaProps<TFieldValues>) {
 	return (
 		<FormField
 			control={control}
 			name={name}
-			render={({ field }) => (
-				<FormItem>
-					{!!label && <FormLabel required={required}>{label}</FormLabel>}
-					<FormControl>
-						<Textarea placeholder={placeholder} rows={rows} {...props} {...field} />
-					</FormControl>
-					<FormMessage />
-				</FormItem>
-			)}
+			render={({ field }) => {
+				const valueLength = field.value?.length || 0;
+				const isOverLimit = maxLength ? valueLength > maxLength : false;
+
+				return (
+					<FormItem>
+						<div className='flex justify-between items-center'>
+							{!!label && <FormLabel required={required}>{label}</FormLabel>}
+							{maxLength && (
+								<div
+									className={cn(
+										'text-xs text-muted-foreground',
+										isOverLimit && 'text-danger font-medium'
+									)}
+								>
+									{valueLength}/{maxLength}
+								</div>
+							)}
+						</div>
+						<FormControl>
+							<Textarea placeholder={placeholder} rows={rows} maxLength={maxLength} {...props} {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				);
+			}}
 		/>
 	);
 }
