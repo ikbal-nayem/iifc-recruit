10b49155753bb4200cdcf419a41d28037435a081
ikbal-nayem <ikbalnayem000@gmail.com>
2025-11-06 14:51:25 +0600

    nn

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -1,27 +1,32 @@
-import { IFile } from "./common.interface";
-import { EnumDTO } from "./master-data.interface";
+import { IFile } from './common.interface';
+import { EnumDTO } from './master-data.interface';
 
-export type UserType = 'SYSTEM' | 'IIFC_ADMIN' | 'JOB_SEEKER' | 'CLIENT' | 'EXAMINER';
+export enum UserType {
+	'SYSTEM',
+	'IIFC_ADMIN',
+	'JOB_SEEKER',
+	'ORG_ADMIN',
+}
 
 export interface IAuthInfo {
-  access_token: string;
-  refresh_token: string;
-  token_type: string;
-  expires_in: number;
+	access_token: string;
+	refresh_token: string;
+	token_type: string;
+	expires_in: number;
 }
 
 export interface IUser {
-  id?: string;
-  username: string;
-  email: string;
-  roles: string[];
-  userType: UserType;
-  firstName: string;
-  lastName: string;
-  fullName?: string;
-  phone?: string;
-  profileImage?: IFile;
-  dateOfBirth?: string;
-  gender?: string;
-  genderDTO?: EnumDTO;
+	id?: string;
+	username: string;
+	email: string;
+	roles: string[];
+	userType: UserType;
+	firstName: string;
+	lastName: string;
+	fullName?: string;
+	phone?: string;
+	profileImage?: IFile;
+	dateOfBirth?: string;
+	gender?: string;
+	genderDTO?: EnumDTO;
 }
