e8e26e01675190ea4832197e12c28ab0eeb6048f
ikbal-nayem <ikbalnayem000@gmail.com>
2025-11-18 21:03:06 +0600

    Revert "redirected you too many times. Maybe middlewire related any issue fix it"
    This reverts commit f943989a6d67c47676e60e5e38e211e320b8963b.

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,12 +10,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-    // Match all pathnames within `/`
-    '/'
-  ]
+  matcher: ['/', '/(bn|en)/:path*']
 };
