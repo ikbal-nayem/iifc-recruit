c9802b5b16daae7181ba1df9384f8601f46d08c2
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-11-18 10:25:01 +0000

    no luck

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,31 +1,47 @@
 'use client';
 
 import Header from '@/components/app/header';
-import SidebarNav from '@/components/app/sidebar-nav';
 import { InterestPromptModal } from '@/components/app/jobseeker/interest-prompt-modal';
+import SidebarNav from '@/components/app/sidebar-nav';
+import { PageLoader } from '@/components/ui/page-loader';
 import { Sidebar, SidebarInset } from '@/components/ui/sidebar';
 import { ROLES } from '@/constants/auth.constant';
+import { ROUTES } from '@/constants/routes.constant';
 import { useAuth } from '@/contexts/auth-context';
+import { useRouter } from 'next/navigation';
 import React, { Suspense, useEffect, useState } from 'react';
 import AuthLayoutProvider from './auth-layout-provider';
-import { PageLoader } from '@/components/ui/page-loader';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-	const { currectUser, clearInterestModalFlag } = useAuth();
+	const { currectUser, clearInterestModalFlag, isAuthenticated, isLoading } = useAuth();
+	const router = useRouter();
 	const [showInterestModal, setShowInterestModal] = useState(false);
 
 	useEffect(() => {
-		// Show interest modal if the flag is set for jobseekers
+		if (!isLoading && !isAuthenticated) {
+			const currentPath = window.location.pathname;
+			const redirectUrl =
+				currentPath === ROUTES.DASHBOARD.ADMIN || currentPath === ROUTES.DASHBOARD.JOB_SEEKER
+					? ''
+					: `?redirectUrl=${encodeURIComponent(currentPath)}`;
+			router.replace(`${ROUTES.AUTH.LOGIN}${redirectUrl}`);
+		}
+	}, [isAuthenticated, isLoading, router]);
+
+	useEffect(() => {
 		if (currectUser?.roles.includes(ROLES.JOB_SEEKER) && currectUser.openInterestModal) {
 			setShowInterestModal(true);
 		}
 	}, [currectUser]);
 
-
 	const handleModalClose = () => {
 		setShowInterestModal(false);
 		clearInterestModalFlag();
 	};
+
+	if (isLoading || !isAuthenticated) {
+		return <PageLoader />;
+	}
 
 	return (
 		<AuthLayoutProvider>
