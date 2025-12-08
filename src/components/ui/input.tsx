83e20faed5ad06863d7cfdbe2109bc8336448eed
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-12-08 15:51:59 +0000

    A component is changing an uncontrolled input to be controlled. This is

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-	({ className, type, ...props }, ref) => {
+	({ className, type, value, ...props }, ref) => {
 		return (
 			<input
 				type={type}
@@ -15,6 +15,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
 					className
 				)}
 				ref={ref}
+				value={value ?? ''}
 				onWheel={(e) => (e.target as HTMLElement).blur()}
 				{...props}
 			/>
